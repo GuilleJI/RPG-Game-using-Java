ed8fef047e240d0d18fea2022ddf21ee75e4f322
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-01-01 19:50:32 -0500

    uploaded images

--- Game Project Java/src/Classes/Cleric.java
+++ Game Project Java/src/Classes/Cleric.java
@@ -1,4 +1,15 @@
 package Classes;
 
-public class Cleric {
+import javax.swing.*;
+
+public class Cleric extends PlayerCharacter {
+
+    //Constructors (private)
+    private ImageIcon clericPic;
+    private String name;
+
+    //Constructors (public)
+    public Cleric(String name, int baseAttack, int defense, int agility, int hitPoints)
+
+
 }
