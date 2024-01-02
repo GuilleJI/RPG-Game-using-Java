9518c1c72699f8239131137455b9d22e8287f0ec
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-01-01 19:00:03 -0500

    PlayerCharacter: created Private and Public Constructors

--- Game Project Java/src/Classes/PlayerCharacter.java
+++ Game Project Java/src/Classes/PlayerCharacter.java
@@ -1,4 +1,41 @@
 package Classes;
 
-public class PlayerCharacter {
+public abstract class PlayerCharacter {
+
+    //Constructors (private)
+    private String name;
+    private Weapon weapon;
+
+    private int baseAttack;
+    private int defense;
+    private int agility;
+    private int hitPoints;
+
+    //Constructors (public)
+
+    public PlayerCharacter(String name, int attack, int weight, int agility, int hitPoints){
+        this.name = name;
+        this.baseAttack = stats[0];
+        this.defense = stats[1];
+        this.agility = stats[2];
+        this.hitPoints = stats[3];
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon){
+        this.weapon = weapon;
+    }
+
+
 }
