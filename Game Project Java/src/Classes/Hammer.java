c5cbeb872fa3a615f02b63e5dd1ae20ec6d01a5a
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-01-02 00:28:39 -0500

    Hammer: private and public Constructors created.

--- Game Project Java/src/Classes/Hammer.java
+++ Game Project Java/src/Classes/Hammer.java
@@ -1,4 +1,39 @@
 package Classes;
 
-public class Hammer {
+import javax.swing.*;
+
+
+public class Hammer extends Weapon {
+
+    //Constructor (private)
+    private ImageIcon hammerPic;
+
+    //Constructor (public)
+
+    public Hammer(int attack, int weight) {
+        super(attack, weight);
+
+        //Set image for this shape
+        hammerPic = new ImageIcon(this.getClass().getResource("/Image/Hammer.jpg"));
+    }
+
+    public ImageIcon getHammerPic() {
+        return hammerPic;
+    }
+
+    public void setHammerPic(ImageIcon hammerPic){
+        this.hammerPic = hammerPic;
+    }
+
+
+    public String getWeaponInformString() {
+        return "The hammer holds a very powerful attack power. " +
+                "A single attack can create a high hit point damage. " +
+                "However, due to it's heavy weight hammers have a slow attack speed. ";
+    }
+
+    public String getCharacterInformString() {
+
+        return null;
+    }
 }
