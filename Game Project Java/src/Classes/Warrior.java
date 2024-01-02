f95b024e47ed9e7cc8d220aaf4e216a86d27470b
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-01-01 23:30:35 -0500

    Warrior: public Constructor created (getter and setter).

--- Game Project Java/src/Classes/Warrior.java
+++ Game Project Java/src/Classes/Warrior.java
@@ -9,4 +9,33 @@ public class Warrior extends PlayerCharacter {
     private ImageIcon monsterPic;
     private String name;
 
+    //Constructors (public):
+    public Warrior(String name, int baseAttack, int defense, int agility, int hitPoints){
+        super(name, baseAttack, defense, agility, hitPoints);
+        warriorPic = new ImageIcon(this.getClass().getResource("/Images/Warrior.jpg"));
+    }
+
+    public ImageIcon getWarriorPic() {
+        return warriorPic;
+    }
+
+    public void setWarriorPic(ImageIcon warriorPic) {
+        this.warriorPic = warriorPic;
+    }
+
+    public ImageIcon getMonsterPic() {
+        return monsterPic;
+    }
+
+    public void setMonsterPic(ImageIcon monsterPic) {
+        this.monsterPic = monsterPic;
+    }
+
+    public String getWeaponInformString() {
+        return null;
+    }
+
+    public String getCharacterInformString() {
+        return "Warriors are engaged or experienced in warfare. They have higher HP and defence but low in agility.";
+    }
 }
