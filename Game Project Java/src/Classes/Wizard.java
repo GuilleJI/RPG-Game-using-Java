d0fa0a02705d1de84549d25779ae9029d7a00d0d
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-01-01 23:45:25 -0500

    Warrior: private and public Constructor created (getter and setter).

--- Game Project Java/src/Classes/Wizard.java
+++ Game Project Java/src/Classes/Wizard.java
@@ -1,4 +1,35 @@
 package Classes;
 
-public class Wizard {
+import javax.swing.*;
+
+public class Wizard extends PlayerCharacter {
+
+    //Constructor (private)
+    private ImageIcon wizardPic;
+    private String name;
+
+    //Constructor (public)
+    public Wizard(String name, int baseAttack, int defense, int agility, int hitPoints){
+        super(name, baseAttack, defense, agility, hitPoints);
+
+        //Set image for this shape
+        wizardPic = new ImageIcon(this.getClass().getResource("/Images/Wizard.jpg"));
+    }
+
+    public ImageIcon getWizardPic() {
+        return wizardPic;
+    }
+
+    public void setWizardPic(ImageIcon wizardPic) {
+        this.wizardPic = wizardPic;
+    }
+
+
+    public String getWeaponInformString() {
+        return null;
+    }
+
+    public String getCharacterInformString(){
+        return "Wizards are wise men who can perform magic spells. They have higher hit points but low in defense.";
+    }
 }
