0e9f8a2c45bbcb5ec0261b0a4fc199626f0c25e3
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-01-02 00:38:20 -0500

    Sword: private and public Constructors created.

--- Game Project Java/src/Classes/Sword.java
+++ Game Project Java/src/Classes/Sword.java
@@ -1,4 +1,39 @@
 package Classes;
 
-public class Sword {
+
+import javax.swing.*;
+
+public class Sword extends Weapon{
+
+    //Constructor (private)
+    private ImageIcon swordPic;
+
+    //Constructor (public)
+    public Sword (int attack, int weight) {
+        super(attack, weight);
+
+        //Set image for its shape
+        swordPic = new ImageIcon(this.getClass().getResource("/Image/Sword.jpg"));
+    }
+
+    public ImageIcon getSwordPic() {
+        return swordPic;
+    }
+
+    public void setSwordPic(ImageIcon swordPic){
+        this.swordPic = swordPic;
+    }
+
+    public String getWeaponInformString() {
+        return "A sword is a versatile good all-round weapon. " +
+                "It is not bulky nor heavy, but inflicts a lot of damage.";
+
+    }
+
+    public String getCharacterInformString() {
+
+        return null;
+    }
+
+
 }
