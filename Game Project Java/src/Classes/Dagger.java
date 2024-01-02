a0bef4e372c3f116b915d1280bcfe89124d0cc62
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-01-02 00:13:18 -0500

    Dagger: private and public Constructors created.

--- Game Project Java/src/Classes/Dagger.java
+++ Game Project Java/src/Classes/Dagger.java
@@ -1,4 +1,34 @@
 package Classes;
 
-public class Dagger {
+import javax.swing.*;
+
+public class Dagger extends Weapon {
+
+    //Constructors (private)
+    private ImageIcon daggerPic;
+
+    //Constructors (public)
+    public Dagger(int attack, int weight){
+        super(attack, weight);
+
+        //Set image for this shape
+        daggerPic = new ImageIcon(this.getClass().getResource("/Image/Dagger.jpg"));
+    }
+
+    public ImageIcon getDaggerPic() {
+        return daggerPic;
+    }
+    public void setDaggerPic(ImageIcon daggerPic){
+        this.daggerPic = daggerPic;
+    }
+
+    public String getCharacterInformString() {
+        return null;
+    }
+
+    public String getWeaponInformString(){
+        return "A dagger is a light and fast weapon. " +
+                "Classes with high agility are very effective when using this weapon. " +
+                "It can maintain fast attack speed, but the increase in additional attack power is not high..";
+    }
 }
