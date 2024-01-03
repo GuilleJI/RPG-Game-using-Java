5d2fadbc5e7d34efb4673f25782ffd1eb206b5fe
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-01-03 18:08:21 -0500

    Adding constructors (public): Creating and setting font for the text

--- Game Project Java/src/GUI/DisplayPanel.java
+++ Game Project Java/src/GUI/DisplayPanel.java
@@ -5,8 +5,11 @@ import Classes.Warrior;
 import Classes.Wizard;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class DisplayPanel extends JPanel {
+
+    //Constructors (private)
     private Warrior warrior;
     private Wizard wizard;
     private Cleric cleric;
@@ -17,5 +20,12 @@ public class DisplayPanel extends JPanel {
     private SecondFrame secondFrame;
 
     private Boolean initial = true;
+
+    //Constructors (public)
+    public DisplayPanel(SecondFrame secondFrame){
+        this.secondFrame = secondFrame;
+        //Create and set font for the text
+        Font myFont = new Font ("Baskerville", Font.BOLD, 20);
+    }
 
 }
