22bf7853576396ee71ca672112f6660c3fa43df2
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-01-03 19:46:58 -0500

    Creating display screen + setting frame properties

--- Game Project Java/src/GUI/SecondFrame.java
+++ Game Project Java/src/GUI/SecondFrame.java
@@ -3,6 +3,7 @@ package GUI;
 import Classes.*;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SecondFrame extends JFrame {
 
@@ -17,5 +18,15 @@ public class SecondFrame extends JFrame {
     private static Monster theMonster;
 
     private int[] data;
+
+    //Constructors (public)
+    public SecondFrame() {
+        //Create display screen
+        //Set Frame Properties
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(200, 200, 750, 800);
+        setTitle("DND Game!");
+        setLayout(new CardLayout());
+    }
 
 }
