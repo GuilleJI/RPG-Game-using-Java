546bab9bfb97c4931a398d5eba05a46434b0f8b7
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-01-03 20:13:26 -0500

    toString method to return a description of the object (Wizard)

--- Game Project Java/src/Classes/Wizard.java
+++ Game Project Java/src/Classes/Wizard.java
@@ -1,5 +1,7 @@
 package Classes;
 
+import GUI.ChoosePanel;
+
 import javax.swing.*;
 
 public class Wizard extends PlayerCharacter {
@@ -24,6 +26,34 @@ public class Wizard extends PlayerCharacter {
     public void setWizardPic(ImageIcon wizardPic) {
 
         this.wizardPic = wizardPic;
+    }
+
+    //toString method to return a description of the object.
+    public String toString() {
+        int[] stats = ChoosePanel.getTextFieldsData();
+        int[] monsStats = ChoosePanel.getSelectMonster();
+        String monsterName = ChoosePanel.getMonsterName();
+        return String.format("Player : %-10s\n---------------------" +
+                        "\nClass  : %-10s" +
+                        "\nHP     : %-3d               Attack Point : %-3d          Defense : %-3d          Agility : %-3d" +
+                        "\nWeapon : %-13s Weight : %-3d               Attack Mod : %-3d" +
+                        "\n\n" +
+
+                        "Monster Name : %-10s\n---------------------" +
+                        "\nHP       : %-3d               Defence     : %-3d          Base Attack : %-3d",
+                name,
+                ChoosePanel.getCharacterType(),
+                100,
+                stats[0],
+                stats[1],
+                stats[2],
+                ChoosePanel.getWeaponType(),
+                stats[5],
+                stats[4],
+                monsterName,
+                50,
+                monsStats[0],
+                monsStats[1]);
     }
 
 
