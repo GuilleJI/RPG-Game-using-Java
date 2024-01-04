80b57cf1a61fffd15d46fa0c244b3f10e9f628e5
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-01-03 19:44:20 -0500

    SecondFrame: Private constructors created

--- Game Project Java/src/GUI/SecondFrame.java
+++ Game Project Java/src/GUI/SecondFrame.java
@@ -1,4 +1,21 @@
 package GUI;
 
-public class SecondFrame {
+import Classes.*;
+
+import javax.swing.*;
+
+public class SecondFrame extends JFrame {
+
+    //Constructors (private)
+    private static Warrior theWarrior;
+    private static Wizard theWizard;
+    private static Cleric theCleric;
+    private static Dagger theDagger;
+    private static Sword theSword;
+    private static Hammer theHammer;
+
+    private static Monster theMonster;
+
+    private int[] data;
+
 }
