2e0c98936d5fd3f5dd7b564c918e0de8607b91e0
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-01-03 19:30:06 -0500

    Main frame created: added setTitle, titleLabel, and setting background color

--- Game Project Java/src/GUI/MainFrame.java
+++ Game Project Java/src/GUI/MainFrame.java
@@ -1,4 +1,24 @@
 package GUI;
 
-public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(200,200, 750,750);
+        setTitle("Crown of Eldoria: Chronicles of the Enchanted Realm");
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+
+        JLabel titleLabel = new JLabel("Crown of Eldoria: Chronicles of the Enchanted Realm", SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Blackmoor LET", Font.BOLD, 30));
+        mainPanel.add(titleLabel, BorderLayout.NORTH);
+
+        JPanel subPanel = new JPanel();
+        subPanel.setLayout(new BoxLayout(subPanel, BoxLayout.Y_AXIS));
+        subPanel.setBackground(Color.LIGHT_GRAY);
+
+    }
 }
